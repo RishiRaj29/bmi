7d70b12f9db0d98ba5fc11c88124e500acd7cc2e
Masquerade <83944275+RishiRaj29@users.noreply.github.com>
2023-07-26 13:51:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './style.css';
+
+
 
 function App() {
+
+  const [weight, setWeight] = useState(30);
+  const [height, setHeight] = useState(120);
+  const [ans, setAns] = useState(21);
+
+  function displayWeight(e) {
+    setWeight(e.target.value);
+  }
+
+  function displayHeight(e) {
+    setHeight(e.target.value);
+  }
+
+  function calculation(e) {
+    // setWeight(e.target.value);
+    // setHeight(e.target.value);
+    var h = (height * height) / 10000;
+    var b = weight / h;
+    setAns(b);
+  }
+
+  function fun1(e) {
+    displayWeight(e);
+    calculation(e);
+  }
+
+  function fun2(e) {
+    displayHeight(e);
+    calculation(e);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <div className='header'>
+          <h2>BMI CALCULATOR</h2>
+        </div>
+        <div className='info'>
+          <label>Weight : {weight}Kg</label><br />
+          <input type='range' min='30' max='200' onInput={fun1}></input><br />
+          <label>Height : {height}cm</label><br />
+          <input type='range' min='120' max='300' onInput={fun2}></input><br />
+          <label>Your BMI is : {ans}</label>
+        </div>
+      </div>
     </div>
   );
 }
